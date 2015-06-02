7c1b2cad40d0559991e26483ef9b9e6cd9045157
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-06-01 22:13:16 -0500

    lynda.com

--- js/app.js
+++ js/app.js
@@ -3,8 +3,23 @@
  */
 
 
-var app = angular.module ('myApp', []);
-app.controller('MyController', function($scope){
-      $scope.myName = ('Jana');
+var myApp = angular.module ('myApp', []);
+    myApp.controller('MyController', function($scope){
+          $scope.myName = ('Jana');
 
 });
+
+var anotherApp = angular.module('anotherApp', []);
+    anotherApp.controller('AuthorController', function($scope) {
+        $scope.author = {
+            'aname': 'Jana Siegle',
+            'title': 'Staff Author',
+            'company': 'Full Sail'
+        }
+    });
+
+        angular.element(document).ready(function () {
+            angular.bootstrap(document.getElementById('anotherApp'), ['anotherApp']);
+
+
+    });
