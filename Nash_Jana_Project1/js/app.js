5516c11be9e92980af38ca7af42578cb3d4f003c
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-06-03 15:27:14 -0500

    ...

--- Nash_Jana_Project1/js/app.js
+++ Nash_Jana_Project1/js/app.js
@@ -7,15 +7,15 @@ var myApp = angular.module ('myApp', []);           //names the app and creates 
     myApp.controller('MyController', function($scope){      //make the controller and name
         console.log("in the controller");           //are we in the controller?  y/n
         $scope.newItem;                                   //create new item
-        console.log("about to add" + $scope.newItem + "to our list");
+        console.log("about to add an item to our list");
         //List Items
         $scope.groceries = ['Bread', 'Milk', 'Eggs', 'Cottage Cheese', 'Almond Milk'];  //pre-filled array
 
 
         //Add List Item
         $scope.addItem = function() {
-            var found = $scope.groceries.indexOf("newItem");
-            alert('found');
+            var found = $scope.groceries.indexOf($scope.newItem);
+            console.log(found);
 
 
                 $scope.groceries.push($scope.newItem);
@@ -38,13 +38,15 @@ var myApp = angular.module ('myApp', []);           //names the app and creates 
 
 });
 
-function empty()         //creates function of empty
+function empty(e)         //creates function of empty
 {
     var x;              //var x to be used pulling the addGrocery id and comparing it to "" (null)
     x = document.getElementById("addGrocery").value;
     if (x === "")           //if user hits save with nothing entered in field an alert will pop up
     {
         alert("Please enter a grocery item!");      //alert to tell them to enter their grocery item.
+        return false;
+
     }
 }
 
