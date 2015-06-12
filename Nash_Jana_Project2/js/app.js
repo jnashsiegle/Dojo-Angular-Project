19f5ce8e1b84e64fb601e90d013a03046d81a9da
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-06-12 12:18:34 -0500

    ...

--- Nash_Jana_Project2/js/app.js
+++ Nash_Jana_Project2/js/app.js
@@ -5,27 +5,28 @@
 /*Initiating Start of the App */
 
 var myApp = angular.module("myApp", []);
-myApp.controller("DBController", function($scope,dataService) {
+myApp.controller("DBController", function($scope,dataService) { /*connect myApp to DBController to dataservice*/
 
-    $scope.employees = dataService.getEmployees();
+    $scope.employees = dataService.getEmployees();  /*pull emp info from dataservice*/
     $scope.newEmployee = {};
 
-    $scope.addNewEmployee = function () {
+    $scope.addNewEmployee = function () {       /*function to create new employee w/keys; save to LS*/
         dataService.saveEmployee($scope.newEmployee.name, $scope.newEmployee.position, $scope.newEmployee.email, $scope.newEmployee.street, $scope.newEmployee.city, $scope.newEmployee.state, $scope.newEmployee.zip);
         $scope.newEmployee = {};
     };
 
-    $scope.removeEmp = function(idx){
+    $scope.removeEmp = function(idx){           /*call delete function from data service */
         dataService.removeEmployeeAt(idx);
     };
 
-    $scope.clearIt = function () {
+    $scope.clearIt = function () {              /* call total clear of LS */
         dataService.destroyLocalStorage();
     };
 
-    $scope.reset = function() {
+    $scope.reset = function() {             /*reset form and clear all fields **does not destroy local info in storage*/
         $scope.newEmployee = angular.copy($scope.employees);
         if ($scope.myForm) $scope.myForm.$setPristine();
     };
-    
+
+
 });
--- Nash_Jana_Project2/js/dataService.js
+++ Nash_Jana_Project2/js/dataService.js
@@ -1,18 +1,18 @@
 /**Created by Jana Nash-Siegle, June 7, 2015 */
 
-angular.module("myApp").service("dataService", function(){
+angular.module("myApp").service("dataService", function(){  /*initiating dataservice file within app*/
 
-    var employeeArray = [];
+    var employeeArray = [];         /*setting var array from LocalStorage */
 
     this.getEmployees = function(){
-        var empArray = JSON.parse(localStorage.getItem("employeesLS")) || [];
+        var empArray = JSON.parse(localStorage.getItem("employeesLS")) || []; /*runs through Local Storage and creates array*/
         employeeArray = empArray;
             console.log(empArray);
         return employeeArray;
     };
 
     this.saveEmployee = function(pName, pPosition, pEmail, pStreet, pCity, pState, pZip){
-        var savedEmployees = {
+        var savedEmployees = {          /*keys for employee information*/
             name:       pName,
             position:   pPosition,
             email:      pEmail, 
@@ -21,17 +21,17 @@ angular.module("myApp").service("dataService", function(){
             state:      pState,
             zip:        pZip
         };
-        employeeArray.push(savedEmployees);
-        localStorage.setItem("employeesLS", JSON.stringify(employeeArray));
+        employeeArray.push(savedEmployees);             /*save new employee information from form*/
+        localStorage.setItem("employeesLS", JSON.stringify(employeeArray));  /*sets it in LS */
     };
 
-    this.removeEmployeeAt = function(pIndex){
-        employeeArray.splice(pIndex,1);
-        localStorage.setItem("employeesLS", JSON.stringify(employeeArray))
+    this.removeEmployeeAt = function(pIndex){   /*function to delete employee information*/
+        employeeArray.splice(pIndex,1);         /*run through array, and delete indexed item*/
+        localStorage.setItem("employeesLS", JSON.stringify(employeeArray)) /*set array in a string*/
     };
 
-    this.destroyLocalStorage = function(){
-        employeeArray.splice(0);
+    this.destroyLocalStorage = function(){      /*total clear of all within LS */
+        employeeArray.splice(0);                /*splicing back from nothing*/
         localStorage.clear();
     };
       
