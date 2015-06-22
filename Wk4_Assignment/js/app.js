9200f12b96272362eef4d2ee2240bb51ea1cc51d
Jana Nash-Siegle <jsiegle@jlsdesk.com>
2015-06-22 14:05:58 -0500

    Dojo Trial for Wk 4 Assignment

--- Wk4_Assignment/js/app.js
+++ Wk4_Assignment/js/app.js
@@ -1,25 +1,6 @@
 /**
  * Created by janasiegle on 6/1/15.
+ Testing DOJO
  */
 
 
-var myApp = angular.module ('myApp', []);
-    myApp.controller('MyController', function($scope){
-          $scope.myName = ('Jana');
-
-});
-
-var anotherApp = angular.module('anotherApp', []);
-    anotherApp.controller('AuthorController', function($scope) {
-        $scope.author = {
-            'aname': 'Jana Siegle',
-            'title': 'Staff Author',
-            'company': 'Full Sail'
-        }
-    });
-
-        angular.element(document).ready(function () {
-            angular.bootstrap(document.getElementById('anotherApp'), ['anotherApp']);
-
-
-    });
